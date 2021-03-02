63f7ca1129f783c25d0db29da754860172f110da
MrAmericanMike <american2010@hotmail.com>
2021-03-02 08:46:45 -0300

    Hello Svelte

---
+++ src/components/Hello.svelte
@@ -0,0 +1,22 @@
+<svelte:options tag="hello-svelte" />
+
+<script>
+	export let _data = JSON.stringify({ cheer: "World" });
+	let data = JSON.parse(_data);
+</script>
+
+<main>
+	<h1>¡Hello {data.cheer}!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		color: aliceblue;
+	}
+	h1 {
+		color: royalblue;
+		text-transform: uppercase;
+		font-size: 6em;
+	}
+</style>
